cd5f764c02aab9d68d60d04f0154418627cfdebe
Debakor Acharjee <debakor10@gmail.com>
2023-06-20 19:59:36 +0600

    sign in update

---
+++ src/screens/SignIn.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet,View, Text, Image, TextInput} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import login from '../../assets/login.png'
+import Button from "../components/Button";
+
+
+export default function SignIn() {
+    return (
+        <SafeAreaView>
+            <Image style={styles.loginImage} source={login} />
+            <Text style={styles.loginTitle}>Login</Text>
+            <View>
+                <TextInput placeholder="Email ID" style={styles.textInput} />
+                <TextInput placeholder="Password" style={styles.textInput} />
+             <Button title="Login" customStyles={{alignSelf:'center', marginTop:20}}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    loginImage: {
+        width: 350,
+        height: 250,
+        flexDirection:'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:80
+    },
+    loginTitle: {
+        fontSize: 28,
+        marginHorizontal: 30,
+        marginVertical: 30,
+    },
+    textInput: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        marginHorizontal: 30, 
+        marginVertical:10
+    },
+    
+  });
+  
