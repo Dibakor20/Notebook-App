95838bf8c6bed5fcf679bc0af6f6896b087160cf
Debakor Acharjee <debakor10@gmail.com>
2023-06-21 23:40:53 +0600

    update

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,45 +1,51 @@
 import React from "react";
-import { StyleSheet,View, Text, Image, TextInput} from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import login from '../../assets/login.png'
+import login from "../../assets/login.png";
 import Button from "../components/Button";
 
-
 export default function SignIn() {
-    return (
-        <SafeAreaView>
-            <Image style={styles.loginImage} source={login} />
-            <Text style={styles.loginTitle}>Login</Text>
-            <View>
-                <TextInput placeholder="Email ID" style={styles.textInput} />
-                <TextInput placeholder="Password" style={styles.textInput} />
-             <Button title="Login" customStyles={{alignSelf:'center', marginTop:20}}/>
-            </View>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView>
+      <Image style={styles.loginImage} source={login} />
+      <Text style={styles.loginTitle}>Login</Text>
+      <View>
+        <TextInput placeholder="Email ID" style={styles.textInput} />
+        <TextInput placeholder="Password" style={styles.textInput} />
+        <Button
+          title="Login"
+          customStyles={{ alignSelf: "center", marginTop: 20 }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    loginImage: {
-        width: 350,
-        height: 250,
-        flexDirection:'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop:80
-    },
-    loginTitle: {
-        fontSize: 28,
-        marginHorizontal: 30,
-        marginVertical: 30,
-    },
-    textInput: {
-        borderBottomWidth: 1,
-        borderBottomColor: 'grey',
-        marginHorizontal: 30, 
-        marginVertical:10
-    },
-    
-  });
-  
+  loginImage: {
+    width: 350,
+    height: 250,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 80,
+  },
+  loginTitle: {
+    fontSize: 28,
+    marginHorizontal: 30,
+    marginVertical: 30,
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+    marginHorizontal: 30,
+    marginVertical: 10,
+  },
+});
